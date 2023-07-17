396bd1d47e6a61fa9fff1cddd784779807ac5fe4
K1ll4a <danilevskiy2609@mail.ru>
2023-07-18 00:40:19 +0300

    Add func day_to_num

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pyowm, datetime
 api_key = '36be05764b5968a7d76c7d9e24e2daab'  # your API Key here as string
 owm = pyowm.OWM(api_key).weather_manager()  # Use API key to get data
 city = input("Enter the name of the city: ")
+day_value = input("Enter day: ")
 
 
 def get_weather():
@@ -13,20 +14,28 @@ def get_weather():
         print_weather(item)
 
 
-def get_day_weather():
+def get_day_weather(day):
     print("***Certain day forecast Weather***")
-    data = get_data()
 
+    day_num = day_to_num(day)
+
+    data = get_data()
     for item in data:
-        print_weather(item)
+        if datetime.datetime.fromtimestamp(item.ref_time).weekday() == day_num:
+            print_weather(item)
+            break
 
 
 def get_current_weather():
     print("***Current day forecast Weather***")
-    data = get_data()
 
+    day_num = day_to_num("today")
+
+    data = get_data()
     for item in data:
-        print_weather(item)
+        if datetime.datetime.fromtimestamp(item.ref_time).weekday() == day_num:
+            print_weather(item)
+            break
 
 
 def get_data():
@@ -72,9 +81,39 @@ def print_weather(data):
     print("\n")
 
 
+def day_to_num(day):
+    lower_day = day.lower()
+
+    if lower_day == "today":
+        return datetime.date.today().weekday()
+
+    if lower_day == "mo":
+        return 0
+
+    if lower_day == "tu":
+        return 1
+
+    if lower_day == "we":
+        return 2
+
+    if lower_day == "th":
+        return 3
+
+    if lower_day == "fr":
+        return 4
+
+    if lower_day == "sa":
+        return 5
+
+    if lower_day == "sun":
+        return 6
+
+    return 0
+
+
 if __name__ == '__main__':
     get_weather()
-    get_day_weather()
+    get_day_weather(day_value)
     get_current_weather()
 
 
